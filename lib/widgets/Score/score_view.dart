44d2c9a95534d2c26df0a4bcb74bf33d5e670428
Joseph Paiz <josephpaiz007@gmail.com>
2024-08-31 12:38:11 -0600

    scoreview

--- lib/widgets/Score/score_view.dart
+++ lib/widgets/Score/score_view.dart
@@ -24,34 +24,95 @@ class _ScoreViewState extends State<ScoreView> {
   @override
   Widget build(BuildContext context) {
     final scoreViewModel = Provider.of<ScoreViewModel>(context);
-    return Expanded(
-      child: Container(
-        width: 300,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Column(
-          children: [
-            const Text(
-              'Score',
-              style: TextStyle(fontSize: 30),
+    return Container(
+      width: 300,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+            color: Colors.black), // Añadir un borde para mejor visibilidad
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Score',
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            'Correct Answers: ${scoreViewModel.correctAnswers}',
+            style: const TextStyle(
+              fontSize: 18,
             ),
-            Text(
-              'Corrects Answers: ${scoreViewModel.correctAnswers}',
-              style: const TextStyle(
-                fontSize: 18,
-              ),
+          ),
+          Text(
+            'Result: ${scoreViewModel.result}',
+            style: const TextStyle(
+              fontSize: 18,
             ),
-            Text(
-              'Result: ${scoreViewModel.result}',
-              style: const TextStyle(
-                fontSize: 18,
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
+
+
+// import 'package:flutter/material.dart';
+// import 'package:plu_trainer/viewmodels/score_view_model.dart';
+// import 'package:plu_trainer/viewmodels/products_view_model.dart';
+// import 'package:provider/provider.dart';
+
+// class ScoreView extends StatefulWidget {
+//   const ScoreView({super.key});
+
+//   @override
+//   State<ScoreView> createState() => _ScoreViewState();
+// }
+
+// class _ScoreViewState extends State<ScoreView> {
+//   @override
+//   void didChangeDependencies() {
+//     super.didChangeDependencies();
+//     final scoreViewModel = Provider.of<ScoreViewModel>(context);
+//     final productViewModel = Provider.of<ProductViewModel>(context);
+
+//     scoreViewModel.updateLists(
+//         productViewModel.products, productViewModel.results);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final scoreViewModel = Provider.of<ScoreViewModel>(context);
+//     return Expanded(
+//       child: Container(
+//         width: 300,
+//         decoration: BoxDecoration(
+//           color: Colors.white,
+//           borderRadius: BorderRadius.circular(20),
+//         ),
+//         child: Column(
+//           children: [
+//             const Text(
+//               'Score',
+//               style: TextStyle(fontSize: 30),
+//             ),
+//             Text(
+//               'Corrects Answers: ${scoreViewModel.correctAnswers}',
+//               style: const TextStyle(
+//                 fontSize: 18,
+//               ),
+//             ),
+//             Text(
+//               'Result: ${scoreViewModel.result}',
+//               style: const TextStyle(
+//                 fontSize: 18,
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
