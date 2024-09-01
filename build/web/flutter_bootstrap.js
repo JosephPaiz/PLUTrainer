1355a2f833f73fc017973345b45697807f4190c6
Joseph Paiz <josephpaiz007@gmail.com>
2024-09-01 00:49:08 -0600

    New upgrades and fixed bugs

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "769429022"
+    serviceWorkerVersion: "2528314821"
   }
 });
