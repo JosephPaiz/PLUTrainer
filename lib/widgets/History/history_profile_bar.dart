422eb6d1e4e0b82665738ca76a62e3f40a5ebb44
Joseph Paiz <josephpaiz007@gmail.com>
2024-09-07 12:16:46 -0600

    correction

--- lib/widgets/History/history_profile_bar.dart
+++ lib/widgets/History/history_profile_bar.dart
@@ -12,119 +12,121 @@ class HistoryProfileBar extends StatefulWidget {
 class _HistoryProfileTextState extends State<HistoryProfileBar> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        // Usamos Expanded para que todos los elementos tengan el mismo ancho
+    return Expanded(
+      child: Row(
+        children: [
+          // Usamos Expanded para que todos los elementos tengan el mismo ancho
 
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(7),
-            child: Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: CustomColors.deepGrey,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Center(
-                child: Text(
-                  'Resultado',
-                  style: Fonts.historyProfileText,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(7),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: CustomColors.deepGrey,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Text(
+                    'Resultado',
+                    style: Fonts.historyProfileText,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(7),
-            child: Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: CustomColors.deepGrey,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Center(
-                child: Text(
-                  'Respuestas contestadas',
-                  style: Fonts.historyProfileText,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(7),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: CustomColors.deepGrey,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Text(
+                    'Respuestas contestadas',
+                    style: Fonts.historyProfileText,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(7),
-            child: Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: CustomColors.deepGrey,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Center(
-                child: Text(
-                  'Respuestas correctas',
-                  style: Fonts.historyProfileText,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(7),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: CustomColors.deepGrey,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Text(
+                    'Respuestas correctas',
+                    style: Fonts.historyProfileText,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(7),
-            child: Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: CustomColors.deepGrey,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Center(
-                child: Text(
-                  'Usos del PLUHelper',
-                  style: Fonts.historyProfileText,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(7),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: CustomColors.deepGrey,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Text(
+                    'Usos del PLUHelper',
+                    style: Fonts.historyProfileText,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(7),
-            child: Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: CustomColors.deepGrey,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Center(
-                child: Text(
-                  'Duración',
-                  style: Fonts.historyProfileText,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(7),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: CustomColors.deepGrey,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Text(
+                    'Duración',
+                    style: Fonts.historyProfileText,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(7),
-            child: Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: CustomColors.deepGrey,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Center(
-                child: Text(
-                  'Tipo de entrenamiento',
-                  style: Fonts.historyProfileText,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(7),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: CustomColors.deepGrey,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Text(
+                    'Tipo de entrenamiento',
+                    style: Fonts.historyProfileText,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/widgets/History/history_profile_layout.dart
+++ lib/widgets/History/history_profile_layout.dart
@@ -28,43 +28,47 @@ class _HistoryProfileLayoutState extends State<HistoryProfileLayout> {
 
     return historyViewMode.isLoading
         ? const Center(child: CircularProgressIndicator())
-        : Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    historyViewMode.profileName ?? 'Unknown',
-                    style: Fonts.historyViewTitleTextStyle,
+        : Expanded(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      historyViewMode.profileName ?? 'Unknown',
+                      style: Fonts.historyViewTitleTextStyle,
+                    ),
                   ),
                 ),
-              ),
-              const HistoryProfileBar(),
-              Expanded(
-                child: historyViewMode.historyList.isEmpty
-                    ? Center(
-                        child: Text(
-                          'No hay un historial disponible',
-                          style: Fonts.noHistoryExistTextStyle,
+                const HistoryProfileBar(),
+                Expanded(
+                  child: historyViewMode.historyList.isEmpty
+                      ? Center(
+                          child: Text(
+                            'No hay un historial disponible',
+                            style: Fonts.noHistoryExistTextStyle,
+                          ),
+                        )
+                      : ListView.builder(
+                          itemCount: historyViewMode.historyList.length,
+                          itemBuilder: (context, index) {
+                            final history = historyViewMode.historyList[index];
+                            return HistoryProfileText(
+                                result: history.score.toStringAsFixed(2),
+                                answeredAnswers:
+                                    history.answeredQuestions.toString(),
+                                correctAnswers:
+                                    history.correctAnswers.toString(),
+                                pluHelperUsage:
+                                    history.pluHelperUsage.toString(),
+                                duration: history.duration.toString(),
+                                trainingType: history.trainingType.toString());
+                          },
                         ),
-                      )
-                    : ListView.builder(
-                        itemCount: historyViewMode.historyList.length,
-                        itemBuilder: (context, index) {
-                          final history = historyViewMode.historyList[index];
-                          return HistoryProfileText(
-                              result: history.score.toStringAsFixed(2),
-                              answeredAnswers:
-                                  history.answeredQuestions.toString(),
-                              correctAnswers: history.correctAnswers.toString(),
-                              pluHelperUsage: history.pluHelperUsage.toString(),
-                              duration: history.duration.toString(),
-                              trainingType: history.trainingType.toString());
-                        },
-                      ),
-              ),
-            ],
+                ),
+              ],
+            ),
           );
   }
 }
