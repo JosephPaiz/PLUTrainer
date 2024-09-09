5a682e2e761538b925597e9ade09abe13470cac0
Joseph Paiz <josephpaiz007@gmail.com>
2024-09-08 22:48:04 -0600

    insert in history fixed bug 4 intent

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "896057eb4ab9742239f33c704a3ea01e
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "03962e0472e2f05bd86d2ccf139f4039",
+"flutter_bootstrap.js": "1e5f4d9e7d655fddc980dcda95558dc3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "0ce76c5310da620d4262de2b08d05252",
 "/": "0ce76c5310da620d4262de2b08d05252",
-"main.dart.js": "1b2dc9832f55ee73edb3161eeb4e74f8",
+"main.dart.js": "3cd4318b72d28a3a0022fb55f3375c26",
 "manifest.json": "a73fa2f581075b3f1211f56d19c7064d",
 "version.json": "630dcc82afb0a011ee9011763da05385"};
 // The application shell files that are downloaded before a service worker can
--- lib/viewmodels/score_view_model.dart
+++ lib/viewmodels/score_view_model.dart
@@ -1,188 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:plu_trainer/services/supabase_service.dart';
-// import 'package:logger/logger.dart';
-
-// class ScoreViewModel extends ChangeNotifier {
-//   final Logger _logger = Logger();
-
-//   List<dynamic> _previousProducts = [];
-//   List<bool> _responses = [];
-
-//   int superKey = 0;
-//   int _answeredAnswers = 0;
-//   int _correctAnswers = 0;
-//   int _pluHelperUsage = 0;
-//   String trainingType = '';
-//   int duration = 0;
-//   double _score = 0.0;
-//   bool _shouldInsert = false;
-//   bool _hasInserted = false;
-
-//   final SupabaseService _supabaseService = SupabaseService();
-
-//   // Getters
-//   List<dynamic> get previousProducts => _previousProducts;
-//   List<bool> get responses => _responses;
-//   int get answeredAnswers => _answeredAnswers;
-//   int get correctAnswers => _correctAnswers;
-//   int get pluHelperUsage => _pluHelperUsage;
-//   double get score => _score;
-
-//   void updateData({
-//     required List<dynamic> products,
-//     required List<bool> responses,
-//     required int superKey,
-//     required int duration,
-//     required String trainingType,
-//     required int pluHelperUsage,
-//     required bool shouldInsert,
-//   }) {
-//     _previousProducts = products;
-//     _responses = responses;
-//     this.superKey = superKey;
-//     this.duration = duration;
-//     this.trainingType = trainingType;
-//     _pluHelperUsage = pluHelperUsage;
-
-//     if (shouldInsert != _shouldInsert && shouldInsert == true) {
-//       _hasInserted = false;
-//     }
-
-//     _shouldInsert = shouldInsert;
-
-//     _answeredAnswers = _previousProducts.length;
-//     _correctAnswers = _responses.where((response) => response == true).length;
-//     _score = double.parse(_score.toStringAsFixed(2));
-
-//     _calculateScore();
-
-//     if (_shouldInsert && !_hasInserted) {
-//       insertHistoryInSupabase();
-//     }
-
-//     notifyListeners();
-//   }
-
-//   void _calculateScore() {
-//     if (_answeredAnswers > 0) {
-//       _score = (_correctAnswers / _answeredAnswers) * 100;
-//     } else {
-//       _score = 0.0;
-//     }
-//   }
-
-//   Future<void> insertHistoryInSupabase() async {
-//     try {
-//       await _supabaseService.insertHistory(
-//         superKey,
-//         _score,
-//         _answeredAnswers,
-//         _correctAnswers,
-//         _pluHelperUsage,
-//         trainingType,
-//         duration,
-//       );
-//       _logger.d('History successfully inserted into Supabase.');
-//       _hasInserted = true;
-//     } catch (e) {
-//       _logger.e('Error inserting history into Supabase: $e');
-//     }
-//   }
-
-//   void resetInsertFlag() {
-//     _hasInserted = false;
-//   }
-// }
-
-// import 'package:flutter/material.dart';
-// import 'package:plu_trainer/services/supabase_service.dart';
-// import 'package:logger/logger.dart';
-
-// class ScoreViewModel extends ChangeNotifier {
-//   final Logger _logger = Logger();
-
-//   List<dynamic> _previousProducts = [];
-//   List<bool> _responses = [];
-
-//   int superKey = 0;
-//   int _answeredAnswers = 0;
-//   int _correctAnswers = 0;
-//   int _pluHelperUsage = 0;
-//   String trainingType = '';
-//   int duration = 0;
-//   double _score = 0.0;
-//   bool _shouldInsert = false;
-//   bool realInsert = false;
-
-//   final SupabaseService _supabaseService = SupabaseService();
-
-//   // Getters
-//   List<dynamic> get previousProducts => _previousProducts;
-//   List<bool> get responses => _responses;
-//   int get answeredAnswers => _answeredAnswers;
-//   int get correctAnswers => _correctAnswers;
-//   int get pluHelperUsage => _pluHelperUsage;
-//   double get score => _score;
-
-//   void updateData({
-//     required List<dynamic> products,
-//     required List<bool> responses,
-//     required int superKey,
-//     required int duration,
-//     required String trainingType,
-//     required int pluHelperUsage,
-//     required bool shouldInsert,
-//   }) {
-//     _previousProducts = products;
-//     _responses = responses;
-//     this.superKey = superKey;
-//     this.duration = duration;
-//     this.trainingType = trainingType;
-//     _pluHelperUsage = pluHelperUsage;
-
-//     if (shouldInsert != _shouldInsert) {
-//       _shouldInsert = shouldInsert;
-//       if (_shouldInsert) {
-//         _calculateScore();
-//         insertHistoryInSupabase();
-//       }
-//     }
-
-//     _answeredAnswers = _previousProducts.length;
-//     _correctAnswers = _responses.where((response) => response == true).length;
-//     notifyListeners();
-//   }
-
-//   void _calculateScore() {
-//     if (_answeredAnswers > 0) {
-//       _score = (_correctAnswers / _answeredAnswers) * 100;
-//     } else {
-//       _score = 0.0;
-//     }
-//     _score = double.parse(_score.toStringAsFixed(2));
-//   }
-
-//   Future<void> insertHistoryInSupabase() async {
-//     try {
-//       await _supabaseService.insertHistory(
-//         superKey,
-//         _score,
-//         _answeredAnswers,
-//         _correctAnswers,
-//         _pluHelperUsage,
-//         trainingType,
-//         duration,
-//       );
-//       _logger.d('History successfully inserted into Supabase.');
-
-//       _shouldInsert = false;
-//       notifyListeners();
-//     } catch (e) {
-//       _logger.e('Error inserting history into Supabase: $e');
-//     }
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:plu_trainer/services/supabase_service.dart';
 import 'package:logger/logger.dart';
@@ -200,11 +15,10 @@ class ScoreViewModel extends ChangeNotifier {
   String trainingType = '';
   int duration = 0;
   double _score = 0.0;
-  bool _hasInserted = false; // Nueva variable para controlar la inserción
+  bool _shouldInsert = false;
 
   final SupabaseService _supabaseService = SupabaseService();
 
-  // Getters
   List<dynamic> get previousProducts => _previousProducts;
   List<bool> get responses => _responses;
   int get answeredAnswers => _answeredAnswers;
@@ -219,8 +33,7 @@ class ScoreViewModel extends ChangeNotifier {
     required int duration,
     required String trainingType,
     required int pluHelperUsage,
-    required bool
-        shouldInsert, // Esta variable sigue presente para definir el proceso
+    required bool shouldInsert,
   }) {
     _previousProducts = products;
     _responses = responses;
@@ -229,16 +42,26 @@ class ScoreViewModel extends ChangeNotifier {
     this.trainingType = trainingType;
     _pluHelperUsage = pluHelperUsage;
 
-    // Verificar si `shouldInsert` es true y `_hasInserted` es false
-    if (shouldInsert && !_hasInserted) {
-      _calculateScore();
-      insertHistoryInSupabase();
+    if (shouldInsert && !_shouldInsert) {
+      _shouldInsert = true;
+      _calculateAndInsertData();
+    } else {
+      _shouldInsert = false;
     }
 
-    // Calcular respuestas y puntaje
+    notifyListeners();
+  }
+
+  Future<void> _calculateAndInsertData() async {
     _answeredAnswers = _previousProducts.length;
     _correctAnswers = _responses.where((response) => response == true).length;
-    notifyListeners();
+    _calculateScore();
+
+    if (_answeredAnswers > 0 && _correctAnswers >= 0) {
+      await insertHistoryInSupabase();
+    } else {
+      _logger.e('Data is incomplete or incorrect, skipping insertion.');
+    }
   }
 
   void _calculateScore() {
@@ -252,7 +75,6 @@ class ScoreViewModel extends ChangeNotifier {
 
   Future<void> insertHistoryInSupabase() async {
     try {
-      // Inserción en Supabase
       await _supabaseService.insertHistory(
         superKey,
         _score,
@@ -263,17 +85,8 @@ class ScoreViewModel extends ChangeNotifier {
         duration,
       );
       _logger.d('History successfully inserted into Supabase.');
-
-      // Cambiar _hasInserted a true para evitar múltiples inserciones
-      _hasInserted = true;
-      notifyListeners();
     } catch (e) {
       _logger.e('Error inserting history into Supabase: $e');
     }
-  }
-
-  // Método para reiniciar el proceso de inserción si se necesita
-  void resetInsertFlag() {
-    _hasInserted = false;
   }
 }
