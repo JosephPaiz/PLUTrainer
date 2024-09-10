68ca60bc7413541fe87c62267f3d8d063376dbc6
Joseph Paiz <josephpaiz007@gmail.com>
2024-09-10 00:02:25 -0600

    New build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3377231131"
+    serviceWorkerVersion: "1567332933"
   }
 });
